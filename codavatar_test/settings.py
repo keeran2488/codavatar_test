b46a9cf453e3421545bb1ff457d9c80fc0341512
Kiran Chaudhary <chaudharyk456@gmail.com>
2023-07-03 20:25:59 +0545

    changed authentication class of rf

--- codavatar_test/settings.py
+++ codavatar_test/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "rest_framework",
+    "rest_framework_simplejwt",
 
     "account",
     "telephony",
@@ -73,6 +74,13 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 
 LANGUAGE_CODE = "en-us"
