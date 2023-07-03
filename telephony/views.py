0f3da7bc7a630713c358cdbfbcfdcf6407279e4e
Kiran Chaudhary <chaudharyk456@gmail.com>
2023-07-03 20:44:13 +0545

    pagination and permission

--- telephony/views.py
+++ telephony/views.py
@@ -1,12 +1,16 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from telephony.serializers import VirtualPhoneNumberSerializer
 from telephony.models import VirtualPhoneNumber
-
+from telephony.pagination import StandardPagination
 
 class VirtualPhoneNumberCreateListView(generics.ListCreateAPIView):
+
     queryset = VirtualPhoneNumber.objects.all()
+    pagination_class = StandardPagination
     serializer_class = VirtualPhoneNumberSerializer
+    permission_classes = [IsAuthenticated,]
 
     def get_queryset(self):
         return VirtualPhoneNumber.objects.filter(user=self.request.user)
